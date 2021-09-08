f27da782109de42c46b0ae35cc694e349dcc4bdc
Rokas <rookas.rudzenskas@gmail.com>
2021-09-08 13:26:18 +0300

    Rokas

--- pages/api/feedback/[siteId].js
+++ pages/api/feedback/[siteId].js
@@ -1,11 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import {db} from "@/lib/firebase";
-import {getAllFeedback} from "@/lib/db-admin";
+import {getAllFeedback, getSite} from "@/lib/db-admin";
 
 export default async function handler(req, res) {
     const siteId = req.query.siteId;
     const { feedback, error } = await getAllFeedback(siteId);
+    const { site } = await getSite(siteId);
 
     if(error) {
         res.status(500).json({ error: error });
