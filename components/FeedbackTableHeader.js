a67ffaea03203c9de1735175dbfda3aaa11ef819
Rokas <rookas.rudzenskas@gmail.com>
2021-09-08 13:19:37 +0300

    Rokas

--- components/FeedbackTableHeader.js
+++ components/FeedbackTableHeader.js
@@ -2,16 +2,19 @@ import React from 'react';
 import {Breadcrumb, BreadcrumbItem, BreadcrumbLink, Flex, Heading} from "@chakra-ui/core";
 import AddSiteModal from "@/components/AddSiteModal";
 
-const FeedbackTableHeader = () => {
+const FeedbackTableHeader = ({siteName}) => {
     return (
         <>
             <Breadcrumb>
                 <BreadcrumbItem isCurrentPage>
-                    <BreadcrumbLink color="gray.700" fontSize="sm">Feedback/</BreadcrumbLink>
+                    <BreadcrumbLink color="gray.700" fontSize="sm">Feedback</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbItem isCurrentPage>
+                    <BreadcrumbLink color="gray.700" fontSize="sm">{siteName}</BreadcrumbLink>
                 </BreadcrumbItem>
             </Breadcrumb>
             <Flex >
-                <Heading flexGrow="1" mb={4}>My Feedback</Heading>
+                <Heading flexGrow="1" mb={4}>{siteName}</Heading>
             </Flex>
         </>
     );
