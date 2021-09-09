dab76451a30d6dc3853887c52c2f9b19af9beac3
Rokas <rookas.rudzenskas@gmail.com>
2021-09-09 09:09:51 +0300

    Rokas

--- components/AddSiteModal.js
+++ components/AddSiteModal.js
@@ -1,3 +1,5 @@
+import { useForm } from 'react-hook-form';
+import { mutate } from 'swr';
 import {
     Modal,
     ModalOverlay,
@@ -5,26 +7,25 @@ import {
     ModalHeader,
     ModalFooter,
     ModalBody,
-    ModalCloseButton, Button, FormControl, FormLabel, Input, useDisclosure, Flex, useToast
-} from "@chakra-ui/core";
-import React from "react";
-import {useForm} from "react-hook-form";
-import {createSite} from "@/lib/db";
-import {useAuth} from "@/lib/auth";
-import useSWR, { mutate } from 'swr';
-import fetcher from "../utils/fetcher";
+    ModalCloseButton,
+    FormControl,
+    FormLabel,
+    Button,
+    Input,
+    useToast,
+    useDisclosure
+} from '@chakra-ui/core';
 
+import { createSite } from '@/lib/db';
+import { useAuth } from '@/lib/auth';
 
-
-const InitialFocus = ({children}) => {
-    const { data, error } = useSWR('/api/sites', fetcher);
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const { handleSubmit, register, errors } = useForm();
-    const initialRef = React.useRef();
+const AddSiteModal = ({ children }) => {
     const toast = useToast();
     const auth = useAuth();
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { handleSubmit, register } = useForm();
 
-    const onCreateSite = ({name, url}) => {
+    const onCreateSite = ({ name, url }) => {
         const newSite = {
             authorId: auth.user.uid,
             createdAt: new Date().toISOString(),
@@ -37,36 +38,31 @@ const InitialFocus = ({children}) => {
             }
         };
 
-        const {id} = createSite(newSite);
+        const { id } = createSite(newSite);
         toast({
-            title: "Success!",
-            description: "We've created added your site.",
-            status: "success",
-            position: "bottom",
+            title: 'Success!',
+            description: "We've added your site.",
+            status: 'success',
             duration: 5000,
-            isClosable: true,
+            isClosable: true
         });
-
-        // mutate('api/sites', { sites: [...data.sites, newSite] });
-
         mutate(
             ['/api/sites', auth.user.token],
-            async (data) => ({ sites: [{id, ...newSite}, ...data.sites ]}),
+            async (data) => ({
+                sites: [{ id, ...newSite }, ...data.sites]
+            }),
             false
         );
-
         onClose();
-    }
+    };
 
     return (
         <>
             <Button
-                maxW="200px"
                 onClick={onOpen}
                 backgroundColor="gray.900"
                 color="white"
                 fontWeight="medium"
-                size="md"
                 _hover={{ bg: 'gray.700' }}
                 _active={{
                     bg: 'gray.800',
@@ -75,17 +71,15 @@ const InitialFocus = ({children}) => {
             >
                 {children}
             </Button>
-
-            <Modal rounded="md" isOpen={isOpen} onClose={onClose}>
+            <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
-                <ModalContent rounded="md" as="form" onSubmit={handleSubmit(onCreateSite)}>
+                <ModalContent as="form" onSubmit={handleSubmit(onCreateSite)}>
                     <ModalHeader fontWeight="bold">Add Site</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody pb={6}>
                         <FormControl>
                             <FormLabel>Name</FormLabel>
                             <Input
-                                id="site-input"
                                 placeholder="My site"
                                 name="name"
                                 ref={register({
@@ -97,7 +91,6 @@ const InitialFocus = ({children}) => {
                         <FormControl mt={4}>
                             <FormLabel>Link</FormLabel>
                             <Input
-                                id="link-input"
                                 placeholder="https://website.com"
                                 name="url"
                                 ref={register({
@@ -112,7 +105,6 @@ const InitialFocus = ({children}) => {
                             Cancel
                         </Button>
                         <Button
-                            id="create-site-button"
                             backgroundColor="#99FFFE"
                             color="#194D4C"
                             fontWeight="medium"
@@ -124,8 +116,7 @@ const InitialFocus = ({children}) => {
                 </ModalContent>
             </Modal>
         </>
+    );
+};
 
-    )
-}
-
-export default InitialFocus;
+export default AddSiteModal;
